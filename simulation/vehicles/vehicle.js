f81088ea25c7e20ce0d82292869fd4e0a79d4ee5
Haimonmon <beankor8@gmail.com>
2024-09-22 12:59:33 +0800

    Added Vehicle Generator

--- simulation/vehicles/vehicle.js
+++ simulation/vehicles/vehicle.js
@@ -24,5 +24,55 @@ class Vehicle {
     }
 }
 
+class GenerateVehicle {
+    constructor(trafficVolume, lanes) {
+        this.trafficVolume = trafficVolume // This will be on percentages: 0.10 = 10% example, so by tenths 10,20,30,40,50,60,70,80,90,100 percentages
+        this.vehicleType = ['Car','Car','Car','Car', 'Bus', ,'Car', 'Truck', 'Emergency']
+        this.lanes = lanes // Array also
+        this.baseTimeInterval = 1000; // *1 Seconds
+    }
+
+    pickRandomVehicle() {
+        return this.vehicleType[Math.floor(Math.random() * this.vehicleType.length)]
+    }
+
+    pickRandomLane() {
+        return this.lanes[Math.floor(Math.random() * this.lanes.length)]
+    }
+
+    generate() {
+        const pickedVehicle = this.pickRandomVehicle()
+        const pickedLane = this.pickRandomLane()
+
+        console.log(`${pickedVehicle} taking lane ${pickedLane}`)
+    }
+    
+    adjustTimeInterval() {
+        if (this.trafficVolume <= 0.3) { // ? 10% - 30%
+            return this.baseTimeInterval * 3;
+        } else if (this.trafficVolume <= 0.6) { // ? 40% - 60%
+            return this.baseTimeInterval * 2;
+        } else if (this.trafficVolume <= 0.9) { // ? 70% - 90%
+            return this.baseTimeInterval + 700;
+        }
+
+        return this.baseTimeInterval // ? 100% or Rush Hour
+
+    }
+
+    startGeneration() {
+        const timeInterval = this.adjustTimeInterval()
+        console.log(timeInterval,'Seconds')
+
+        setInterval(() => {
+            this.generate()
+        }, timeInterval)
+    }
+}
+
+if (require.main === module) {
+    const generator = new GenerateVehicle(0.1, [1,2,3,4,5,6,7,8]) // Btw this lane is a crossing , each sides have 2 lanes
+    generator.startGeneration()
+}
 
 module.exports = { Vehicle }
